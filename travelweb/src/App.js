2e28c0fb35d3e70da72d8377e56b14f160b5bc19
sougata2002 <sougatabhowmick62@gmail.com>
2023-10-07 05:33:54 +0530

    Other components added!!

--- travelweb/src/App.js
+++ travelweb/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-// import Footer from './Components/Footer/Footer';
-// import Home from './Components/Home/Home';
-// import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
